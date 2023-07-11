f791bf4a38156809518c1c0f59d14ffa9fd1ed72
prajakta-desale <prajktadesle@opticalarc.com>
2023-07-11 16:13:20 +0530

    input validations are added

--- app/Routes/user/User.route.ts
+++ app/Routes/user/User.route.ts
@@ -4,14 +4,10 @@ import express from "express";
 import { celebrate } from "celebrate";
 import UserSchema from "../../Constants/Schema/User.schema";
 const router = express.Router();
-router.post(
-  "/sign-in",
-  // celebrate(UserSchema.login),
-  userController.login
-);
+router.post("/sign-in", celebrate(UserSchema.login), userController.login);
 router.post(
   "/register",
-  // celebrate(UserSchema.register),
+  celebrate(UserSchema.register),
   userController.register
 );
 export default router;
